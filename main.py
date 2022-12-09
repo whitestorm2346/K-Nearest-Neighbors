acee8e8b25d9c5d231de46ab66dd0d2fc4672682
whitestorm2346 <andrew770426@gmail.com>
2022-12-10 02:45:59 +0800

    Finished defining the KNN class

--- main.py
+++ main.py
@@ -2,57 +2,56 @@ import pandas as pd  # data processing -> CSV file I/O
 import numpy as np  # linear algebra
 from scipy import spatial
 import matplotlib.pyplot as plt  # for data visualization
+from collections import Counter  # for most_common
 # from sklearn.neighbors import KNeighborsClassifier
 
 
-class KNearestNeighbors:
-    def __init__(self, k) -> None:
+def euclidean_distance(x1, x2) -> float:
+    return np.sqrt(np.sum((x1 - x2) ** 2))
+
+
+class KNN:
+    def __init__(self, k=3) -> None:
         self.k = k
 
-    def __init__(self, k, x_data, y_data) -> None:
-        self.k = k
-        self.x_data = x_data
-        self.y_data = y_data
+    def fit(self, x_train, y_train) -> None:
+        self.x_train = x_train
+        self.y_train = y_train
 
-    def set_k(self, k) -> None:
-        self.k = k
+    def predict(self, X) -> list:
+        predicted_labels = [self._predict(x) for x in X]
 
-    def set_data(self, x_data, y_data) -> None:
-        self.x_data = x_data
-        self.y_data = y_data
+        return np.array(predicted_labels)
 
-    def get_distance(self, target_x, target_y) -> None:
-        self.distances = []
+    def _predict(self, x):
+        distances = [euclidean_distance(x, x_train)
+                     for x_train in self.x_train]
 
-        for i in self.x_data:
-            distance = np.sqrt(
-                ((target_x - self.x_data[i]) ** 2) +
-                ((target_y - self.y_data[i]) ** 2)
-            )
-            self.distances.append(distance)
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
 
-    def predict(self, target_x, target_y) -> int:
+        most_common = Counter(k_nearest_labels).most_common(1)
 
-        return 0
+        return most_common[0][0]
 
 
 # load dataset CSV file
 DATA_SET_URL = 'https://raw.githubusercontent.com/whitestorm2346/neural-network/main/train.csv'
-pd_df = pd.read_csv(DATA_SET_URL)
+df = pd.read_csv(DATA_SET_URL)
 
 # data frame for entity name and ID
-pd_df0 = 0
+df0 = 0
 
 # data frame for attributes
-pd_df1 = 0
+df1 = 0
 
 # convert to 0s and 1s based on the attribute values
-pd_df1 = pd_df.iloc[:, [0, 2]]
+df1 = df.iloc[:, [0, 2]]
 
 # merge pd_df0 and pd_df1 data frame
-pd_df2 = pd.concat([pd_df0, pd_df1], axis=1, sort=False)
+df2 = pd.concat([df0, df1], axis=1, sort=False)
 
-df_array = pd_df2.to_numpy()
+df_array = df2.to_numpy()
 
 carDict = {}
 
