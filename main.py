23e77f419dbc5bd7df5628d6e8469f5739b52810
whitestorm2346 <andrew770426@gmail.com>
2022-12-15 01:57:17 +0800

    Finished reading the data from CSV file

--- main.py
+++ main.py
@@ -1,78 +1,90 @@
-import pandas as pd  # data processing -> CSV file I/O
-import numpy as np  # linear algebra
+import operator
+import pandas as pd
+import numpy as np
 from scipy import spatial
 import matplotlib.pyplot as plt  # for data visualization
-from collections import Counter  # for most_common
-# from sklearn.neighbors import KNeighborsClassifier
-
-
-def euclidean_distance(x1, x2) -> float:
-    return np.sqrt(np.sum((x1 - x2) ** 2))
-
-
-class KNN:
-    def __init__(self, k=3) -> None:
-        self.k = k
-
-    def fit(self, x_train, y_train) -> None:
-        self.x_train = x_train
-        self.y_train = y_train
-
-    def predict(self, X) -> list:
-        predicted_labels = [self._predict(x) for x in X]
-
-        return np.array(predicted_labels)
-
-    def _predict(self, x):
-        distances = [euclidean_distance(x, x_train)
-                     for x_train in self.x_train]
-
-        k_indices = np.argsort(distances)[:self.k]
-        k_nearest_labels = [self.y_train[i] for i in k_indices]
-
-        most_common = Counter(k_nearest_labels).most_common(1)
-
-        return most_common[0][0]
-
 
 # load dataset CSV file
-DATA_SET_URL = 'https://raw.githubusercontent.com/whitestorm2346/neural-network/main/train.csv'
-df = pd.read_csv(DATA_SET_URL)
+DATA_SET_URL = 'https://raw.githubusercontent.com/yfujieda/tech-cookbook/master/python/knn-example2/data/car_dataset.csv'
+data_set = pd.read_csv(DATA_SET_URL)
 
-# data frame for entity name and ID
-df0 = 0
 
-# data frame for attributes
-df1 = 0
+def ComputeDistance(a, b):
+    dataA = a[1]
+    dataB = b[1]
+
+    AttributeDistance = spatial.distance.cosine(dataA, dataB)
+
+    return AttributeDistance
+
+
+def getNeighbors(id, k):
+    global car_dict
+
+    distances = []
+    neighbors = []
+
+    for car in car_dict:
+        if car != id:
+            dist = ComputeDistance(car_dict[id], car_dict[car])
+            distances.append((car, dist))
+
+    distances.sort(key=operator.itemgetter(1))
+
+    for x in range(k):
+        neighbors.append((distances[x][0], distances[x][1]))
+
+    return neighbors
+
+
+# dataframe for entity name and ID
+cars = data_set.iloc[:, [0, 2]]
+# model-index | model-name
+
+# print(pd_df0, end='\n\n\n')
+
+# dataframe for attributes
+cars_attributes = data_set.iloc[:, [3, 4, 5, 6, 7, 8]]
+# fuel_type | aspiration | num_of_doors | body_style | drive_wheels | engine_location
+
+# print(pd_df1, end='\n\n\n')
 
 # convert to 0s and 1s based on the attribute values
-df1 = df.iloc[:, [0, 2]]
+cars_attributes = pd.get_dummies(cars_attributes)
+# 將屬性質根據性質轉換為 0 或 1 的表
 
-# merge pd_df0 and pd_df1 data frame
-df2 = pd.concat([df0, df1], axis=1, sort=False)
+# print(pd_df1, end='\n\n\n')
 
-df_array = df2.to_numpy()
+# merge pd_df0 and pd_df1 dataframes
+cars_info = pd.concat([cars, cars_attributes], axis=1, sort=False)
+# 將兩個 dataframe 合併
 
-carDict = {}
+# print(pd_df2, end='\n\n\n')
 
-for d in df_array:
-    pass
+cars_info_array = cars_info.to_numpy()
+# 將 df物件轉型回 array物件
 
-nearest_k = 0  # k個最接近你的鄰居
-selected_id = 0
+# print(df_array, end='\n\n\n')
 
-print("")
+car_dict = {}
 
-neighbors = []
+for car in cars_info_array:
+    index = int(car[0])
+    name = car[1]
+    attributes = car[2:]
+    attributes = map(int, attributes)
+    car_dict[index] = (name, np.array(list(attributes)))
+
+k = 10
+selected_index = 5
+
+print("selected car: ", car_dict[selected_index][0])  # print model name
+
+neighbors = getNeighbors(selected_index, k)
 
 for neighbor in neighbors:
-    print(
-        str(neighbor[0]) + " | "
-        + carDict[neighbor[0]][0] + " | "
-        + str(neighbor[1])
-    )
+    model_index = neighbor[0]
+    model_name = car_dict[model_index][0]
+    model_value = neighbor[1]
 
-# https://godamber.blogspot.com/2019/07/pythoncolab.html(資料集讀取方式，若同學使用colab以外的編譯器需自行搜尋CSV檔案讀取方法)
-# https://ithelp.ithome.com.tw/articles/10269826?sc=iThelpR(簡易KNN(有使用套件))
-# https://www.w3schools.com/python/python_ml_knn.asp(簡易KNN(有使用套件，且有圖示))
-# https://www.kaggle.com/code/prashant111/knn-classifier-tutorial/notebook(比較正規的KNN資料分析，較難)
+    print("%d | %s | %.3f" % (model_index, model_name, model_value))
